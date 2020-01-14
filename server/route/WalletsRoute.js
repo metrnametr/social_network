cf903098b718eab7c5b12328e0538fdf72a2b554
metrnametr <patronius1515@gmail.com>
2020-01-14 12:25:19 +0200

    foo

--- server/route/WalletsRoute.js
+++ server/route/WalletsRoute.js
@@ -1,7 +1,25 @@
 const route = require('express').Router();
 const WalletsModel = require('../models/WalletsModel');
 
+const tunnel = require('tunnel');
+const axios = require('axios');
+
+const request = require('request')
+
+const fs = require('fs');
+
+const tunnelingAgent = tunnel.httpsOverHttp({
+  proxy: {
+    host: 'localhost',
+    port: 8080
+  },
+  headers: {
+    'User-Agent': 'Node'
+  }
+});
+
 route.get('/wallets', async (req, res) => {
+  fetchData();
   try {
     const wallets = await WalletsModel.getWallets();
     res.send(wallets);
@@ -44,14 +62,54 @@ route.delete('/wallets/:id', async (req, res) => {
 route.put('/wallets/:id', async (req, res) => {
   const { id } = req.params;
   const { body } = req;
-  console.log(body)
+  console.log(body);
   try{
-    const wallets = await WalletsModel.putWallet(id, body)
+    const wallets = await WalletsModel.putWallet(id, body);
     res.send(wallets);
   } catch(e) {
     console.log(e);
   }
 });
+
+
+const fetchData = async () => {
+  const post = await axios.get('https://www.tiktok.com/@mariamateu/video/6775097456058256645', {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11',
+    },
+  });
+  // function callback(error, response, body) {
+  //   if (!error && response.statusCode == 200) {
+  //     console.log(body)
+  //   }
+  // }
+  // const post = request({
+  //   url: 'https://www.tiktok.com/@mariamateu/video/6775097456058256645',
+  //   headers: {
+  //     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11',
+  //   }
+  // }, callback)
+  // const startIndex = post.data.search(/video-card default/);
+  const startIndex = post.data.search(/<video playsinline="" loop="" src=/);
+  const data = post.data.slice(startIndex, startIndex + 500);
+  const dataItemLink = data.split('"').find(parseLink => parseLink.match(/https/));
+  
+   request({
+    url: `${dataItemLink}`,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11',
+    }
+  }).pipe(fs.createWriteStream('my_item.mp4'))
+
+  const video = await axios.get(`${dataItemLink}`, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11',
+    },
+  });
+  // const res = await fs.createWriteStream(`${__dirname}/my_item.mp4`, video.data);
+  // console.log(res);
+  // console.log(video.data)
+};
 
 
 module.exports = route;
--- src/containers/WalletsPage/WalletsPage.js
+++ src/containers/WalletsPage/WalletsPage.js
@@ -1,6 +1,7 @@
 import React,   { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import styled from 'styled-components';
+import axios from 'axios';
 import { Layout } from 'antd';
 import WalletsTable from './WalletsTable';
 import WalletItems from './WalletItems';
@@ -43,7 +44,42 @@ const WalletsPage = (props) => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchWallets());
+    // fetchData();
   }, []);
+  // const fetchData = async () => {
+  //   const post = await axios.get('https://v19.muscdn.com/b09bfee27ccb32147e39b4fc4d538162/5e0a58b7/video/tos/maliva/tos-maliva-v-0068/243a8e4a605a4609adfa9500b8ef26cb/?a=1233&br=1152&bt=576&cr=0&cs=0&dr=0&ds=3&er=&l=201912301406030101151761430A4406A7&lr=tiktok_m&qs=0&rc=anhuMzVyPG1wcjMzNDczM0ApZjk0PGc1NGQ5NzdlZmc0ZmcwYS4xZG0tXy9fLS00MTZzc2I2YDA2MGBhLmNhXmAxY2M6Yw%3D%3D', {
+  //     headers: {
+  //       'Accept': '*/*',
+  //       'Accept-Encoding': 'identity;q=1, *;q=0',
+  //       'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6',
+  //       'Connection': 'keep-alive',
+  //       'Host': 'v19.muscdn.com',
+  //       'Range': 'bytes=0-',
+  //       'Sec-Fetch-Mode': 'no-cors',
+  //       'Sec-Fetch-Site': 'same-origin',
+  //       'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36',
+  //       responseType:'stream'
+  //     },
+  //     httpsAgent: agent,
+  //     params: {
+  //       br: 1152,
+  //       bt: 576,
+  //       cr: 0,
+  //       cs: 0,
+  //       dr: 0,
+  //       ds: 3,
+  //       er: '',
+  //       l: '201912301406030101151761430A4406A7',
+  //       lr: 'tiktok_m',
+  //       qs: 0,
+  //       rc: 'anhuMzVyPG1wcjMzNDczM0ApZjk0PGc1NGQ5NzdlZmc0ZmcwYS4xZG0tXy9fLS00MTZzc2I2YDA2MGBhLmNhXmAxY2M6Yw%3D%3D'
+  //     }
+      
+  //   });
+  //   console.log(1);
+  //   console.log(post);
+  //   console.log(post);
+  // };
   if (clearTable)
     return (
       <CreateWalletContainer>
